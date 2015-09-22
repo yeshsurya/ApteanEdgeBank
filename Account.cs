bcca87a98d5157e52618aa4b2ee2b990e53d54e6
ShruthaKashyap <shrutha.kashyap@gmail.com>
2015-09-22 17:17:29 +0530

    Account changer in progress...

--- Account.cs
+++ Account.cs
@@ -1,27 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ApteanEdgeBank
 {
     public abstract class Account
     {
         public int accountID;
+        public string accountType;
         public DateTime dateOpened;
         public DateTime dateClosed;
         public double Balance;
 
-        public abstract void Create(int ID,DateTime open,double balance);
+        public abstract void Create(string accType,DateTime open,double balance);
         public abstract void Deposit(double amount);
-        public void Withdraw(double amount)
+        public void Withdraw(double amount,int accID)
         {
+            UserDAO dao=new UserDAO();
+            DataTable dt=new DataTable();
+            string connectionstring = "Data Source=WS003LT1553PRD;Initial Catalog=ApteanEdgeBank;User=sa;Password=abc-123";
+            dt=dao.GetData("Select AccountBalance from Account where AccountID="+accID,connectionstring);
+            Balance=Convert.ToDouble(dt.Rows[0]["AccountBalance"]);
             Balance = Balance - amount;
             if (Balance > 0)
-                Console.WriteLine("The new balance={0}", Balance);
+            {
+                string myQuery = "Update Account set AccountBalance=" + Balance + " where AccountID in (select AccountID from CustomerAccount)";
+                
+                //UserDAO dao = new UserDAO();
+                dao.UpdateData(myQuery, connectionstring);
+
+            }
+            //string myQuery="up
             else
-                Console.WriteLine("You're account balance is too low!");
+                MessageBox.Show("Your balance is too low!");
         }
         public void Close()
         {
@@ -37,24 +52,54 @@ namespace ApteanEdgeBank
             }
         }
         
+        public bool CheckAccountExistence(int accID)
+        {
+            UserDAO dao = new UserDAO();
+            DataTable accTable = new DataTable();
+            string myQuery = "select * from Account where AccountID=" + accID;
+            string connectionstring="Data Source=WS003LT1553PRD;Initial Catalog=ApteanEdgeBank;User=sa;Password=abc-123";
+            accTable = dao.GetData(myQuery, connectionstring);
+            if (accTable.Rows.Count > 0)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 
     public class ChequingAccount : Account
     {
  
-        public override void Create(int ID,DateTime open,double balance)
+        public override int Create(string accType,DateTime open,double balance)
         {
-            accountID=ID;
+           /* accountID=ID;
             dateOpened=open;
             Balance=balance;
             Console.WriteLine("Chequing Acoount:");
-            Console.WriteLine("Account ID:{0}\nDate Opened:{1}\nBalance:{2}", accountID, dateOpened, Balance);
+            Console.WriteLine("Account ID:{0}\nDate Opened:{1}\nBalance:{2}", accountID, dateOpened, Balance);*/
+
+            string myQuery = "insert into Account values("+"'"+accType+"'"+","+balance+","+"'"+open+"'"+","+"null"+")";
+            string connectionstring = "Data Source=WS003LT1553PRD;Initial Catalog=ApteanEdgeBank;User=sa;Password=abc-123";
+            UserDAO dao = new UserDAO();
+            dao.InsertData(myQuery, connectionstring);
+            DataTable dataTable = dao.GetData("use ApteanEdgeBank select max(AccountID) as max from Account", UserDAO.connectionString);
+            int AccountID = Convert.ToInt32(dataTable.Rows[0]["max"]);
+            return AccountID;
+            //dao.InsertData("use ApteanEdgeBank insert into CustomerAccount values(" + CustomerID + "," + AccountID + ")", UserDAO.connectionString);
         }
         
-        public override void Deposit(double amount)
+        public override void Deposit(double amount,int accId)
     {
+        UserDAO dao=new UserDAO();
+        DataTable dt=new DataTable();
+        string connectionstring = "Data Source=WS003LT1553PRD;Initial Catalog=ApteanEdgeBank;User=sa;Password=abc-123";
+        dt=dao.GetData("Select AccountBalance from Account where AccountID="+accId,connectionstring);
+        Balance=Convert.ToDouble(dt.Rows[0]["AccountBalance"]);
         Balance=Balance+amount;
-        Console.WriteLine("The new balance ={0}", Balance);
+        string myQuery = "Update Account set AccountBalance=" + Balance + " where AccountID in (select AccountID from CustomerAccount)";
+
+        //UserDAO dao = new UserDAO();
+        dao.UpdateData(myQuery, connectionstring);
     }
 
     }
@@ -69,33 +114,51 @@ namespace ApteanEdgeBank
             Limit=5000;
         }
 
-        public override void Create(int ID,DateTime open,double balance)
+        public override int Create(string accType,DateTime open,double balance)
         {
           if (balance <= Limit)
           {
-                accountID = ID;
+                
                 dateOpened = open;
                 Balance = balance;
-                Console.WriteLine("Tax free savings account:");
-                Console.WriteLine("Account ID:{0}\nDate Opened:{1}\nBalance:{2}\nLimit on account={3}", accountID, dateOpened, Balance, Limit);
+                string myQuery = "insert into Account values(" + "'" + accType + "'" + "," + balance + "," + "'" + open + "'" + "," + "null" + ")";
+                string connectionstring = "Data Source=WS003LT1553PRD;Initial Catalog=ApteanEdgeBank;User=sa;Password=abc-123";
+                UserDAO dao = new UserDAO();
+                dao.InsertData(myQuery, connectionstring);
+                DataTable dataTable = dao.GetData("use ApteanEdgeBank select max(AccountID) as max from Account", UserDAO.connectionString);
+                int AccountID = Convert.ToInt32(dataTable.Rows[0]["max"]);
+                return AccountID;
          }
 
          else
             {
-                Console.WriteLine("Balance is greater than limit!");
+                MessageBox.Show("Balance is greater than limit!");
+                return 0;
             } 
         }
         
-        public override void Deposit(double amount)
+        public override void Deposit(double amount,int accID)
     {
+        UserDAO dao = new UserDAO();
+        DataTable dt = new DataTable();
+        string connectionstring = "Data Source=WS003LT1553PRD;Initial Catalog=ApteanEdgeBank;User=sa;Password=abc-123";
+        dt = dao.GetData("Select AccountBalance from Account where AccountID=" + accID, connectionstring);
+        Balance = Convert.ToDouble(dt.Rows[0]["AccountBalance"]);
         if ((Balance + amount) < 5000)
         {
+
             Balance = Balance + amount;
-            Console.WriteLine("The new balance is={0}", Balance);
+            string myQuery = "Update Account set AccountBalance=" + Balance + " where AccountID in (select AccountID from CustomerAccount)";
+
+            //UserDAO dao = new UserDAO();
+            dao.UpdateData(myQuery, connectionstring);
         }
 
         else
-            Console.WriteLine("The uuper limit on the account is Rs5000");
+        {
+            MessageBox.Show("Balance greater than limit!");
+        }
+            
     }
     }
 }
